5420ddb88cbe4f25818e2dd69cf44a0da516744f
Quentin GIRARD <qgirard@e1r11p10.42.fr>
2019-02-20 18:55:37 +0100

    push_swap

--- push_swap/mediane.c
+++ push_swap/mediane.c
@@ -6,13 +6,13 @@
 /*   By: qgirard <qgirard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 13:42:54 by qgirard           #+#    #+#             */
-/*   Updated: 2019/02/20 17:28:09 by qgirard          ###   ########.fr       */
+/*   Updated: 2019/02/20 18:53:07 by qgirard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int		findmed(int **tab, int pivot)
+int		findmed(int **tab, int pivot, int var)
 {
 	int		i;
 	int		j;
@@ -29,11 +29,12 @@ int		findmed(int **tab, int pivot)
 		}
 		j++;
 	}
-	ft_swap(&((*tab)[i + 1]), &((*tab)[pivot]));
-	if (i - 1 >= 0 && i - 1 != pivot)
-		findmed(tab, i - 1);
-	if (i + 1 < pivot)
-		findmed(tab, i + 1);
+	if ((*tab)[i + 1] > (*tab)[pivot])
+		ft_swap(&((*tab)[i + 1]), &((*tab)[pivot]));
+	if (i >= 0 && var > 0)
+		findmed(tab, i, 2);
+	if (i + 2 < pivot && (var == 0 || var == 1))
+		findmed(tab, i + 2, 0);
 	return (pivot / 2);
 }
 
@@ -54,14 +55,14 @@ int		mediane(t_lcheck **stock)
 		k++;
 		tmp = tmp->next;
 	}
-	k = tab[findmed(&tab, countlist(stock) - 1)];
-	/*k = 0;
+	k = tab[findmed(&tab, countlist(stock) - 1, 1)];
+	k = 0;
 	while (k < countlist(stock))
 	{
 		ft_printf("%d\n", tab[k]);
 		k++;
-	}*/
-	ft_printf("%d\n", k);
+	}
+//	ft_printf("%d\n", k);
 	free(tab);
 	return (k);
 }
